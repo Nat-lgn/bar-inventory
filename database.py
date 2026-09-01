1de928852907dd7106a4dd8c4e15d612c29fd764
Nat-lgn <109870447+Nat-lgn@users.noreply.github.com>
2026-09-02 01:08:30 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine('sqlite:///database.db', echo=True)
+# Проверяем, где мы запущены. В Streamlit Cloud папка /mount/src/ существует.
+# Если мы в облаке, сохраняем базу в безопасное место, иначе — локально в папку проекта.
+if os.path.exists("/mount/src"):
+    DB_PATH = "sqlite:////mount/src/bar-inventory/database.db"
+else:
+    DB_PATH = "sqlite:///database.db"
+
+engine = create_engine(DB_PATH, echo=True)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
