6eae2c9494ac990ed0dd0e68e210c3631d546a5c
Nat-lgn <109870447+Nat-lgn@users.noreply.github.com>
2026-09-05 01:21:23 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,14 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
 from database import Base  # Импортируем готовый Base из базы данных
+import streamlit as st
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = st.secrets["database"]["url"]
+
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
 
 class Product(Base):
     __tablename__ = "products"
