3ccffc2940d69e91d00244670f3dc4c31d762b77
Nat-lgn <109870447+Nat-lgn@users.noreply.github.com>
2026-09-05 01:23:02 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Считываем строку подключения из секретов Streamlit
 DATABASE_URL = st.secrets["database"]["url"]
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
+Base = declarative_base()
