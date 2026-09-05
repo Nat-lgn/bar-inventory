7ec21e927ea4695e999103cfb15b89cc01f2b267
Nat-lgn <109870447+Nat-lgn@users.noreply.github.com>
2026-09-05 03:40:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -192,7 +192,7 @@ if page == t["p1"]:
                                 value=str(p_data.get("val_str", "")), 
                                 key=f"val_str_{p.id}",
                                 label_visibility="collapsed",
-                                placeholder="Кол-во / формула"
+                                placeholder="Кол-во"
                             )
                         val = evaluate_expression(val_input)
                         with col_res:
